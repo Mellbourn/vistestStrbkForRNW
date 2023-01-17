b836452cd548274eea489bc8389c9f2aa7c65f30
Klas Mellbourn <klas@mellbourn.net>
2023-01-17 14:06:59 +0100

    add @storybook/addon-react-native-web to main

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,13 @@
 module.exports = {
-  "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: [
+    '../stories/**/*.stories.mdx',
+    '../stories/**/*.stories.@(js|jsx|ts|tsx)',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/addon-react-native-web',
   ],
-  "framework": "@storybook/react"
-}
+  framework: '@storybook/react',
+};
