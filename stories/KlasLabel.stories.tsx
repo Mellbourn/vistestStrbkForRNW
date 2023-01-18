3e6841fc4c73885b82c93c66399953dc98f1fe37
Klas Mellbourn <klas@mellbourn.net>
2023-01-18 12:49:17 +0100

    trying to change chromatic viewport

--- stories/KlasLabel.stories.tsx
+++ stories/KlasLabel.stories.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {INITIAL_VIEWPORTS} from '@storybook/addon-viewport';
 
 import {KlasLabel} from '../KlasLabel';
 
@@ -8,19 +9,14 @@ export default {
   component: KlasLabel,
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {},
+  parameters: {
+    chromatic: {
+      viewport: {
+        viewports: INITIAL_VIEWPORTS,
+        defaultViewport: 'iphone6',
+      },
+    },
+  },
 };
-
-// // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-// const Template = (args: any) => <KlasLabel {...args} />;
-//
-// export const Primary = Template.bind({});
-// // More on args: https://storybook.js.org/docs/react/writing-stories/args
-// (Primary as any).args = {};
-
-// import {storiesOf} from '@storybook/react-native';
-//
-// const props = {};
-//
-// storiesOf('App', module).add('Primary', () => <KlasLabel />);
 
 export const Primary = <KlasLabel />;
